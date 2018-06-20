899b893a9342600921ea97bffe3fc4921a4d65d5
sunyjglog <18332552245@163.com>
2018-06-20 15:26:54 +0800

    提交分支

--- src/com/ronglian/Apple.java
+++ src/com/ronglian/Apple.java
@@ -3,4 +3,8 @@ package com.ronglian;
 public class Apple {
 	private String name;
 	private String color;
+	
+	public static void main(String[] args) {
+		System.out.println("feature-sunyj-0.0.1.....");
+	}
 }
